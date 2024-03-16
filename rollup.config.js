8853b92eb3575f8b98537ba044ea3f002e4baf8f
Aneena John Xavier <axavier@atlassian.com>
2024-03-16 16:37:55 +0530

    Integrate roll up configs
    Signed-off-by: Aneena John Xavier <axavier@atlassian.com>

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+import postcss from 'rollup-plugin-postcss';
+import packageJson from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/bundle.min.js',
+        format: 'iife',
+        name: 'version',
+        plugins: [terser()],
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(), //DOCS: Resolves third-party module dependencies.
+      commonjs(), //DOCS: Converts CommonJS modules to ESM.
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'],
+      }), //DOCS:  Compiles TypeScript files, excluding test files and Storybook stories.
+      postcss({ extensions: ['.css'], inject: true, extract: false }), //DOCS: Processes CSS files with PostCSS, injecting the styles into the bundled JavaScript.
+    ],
+  },
+  //   DOCS: Generating a TypeScript declaration file (.d.ts) for a library or package is essential
+  //     - providing better type information
+  //     - improving the devX when consuming the library in other TypeScript projects.
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/], //Excludes CSS files from being bundled.
+  },
+];
